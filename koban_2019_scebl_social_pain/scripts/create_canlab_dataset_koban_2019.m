1eca86610b6b783174ae958bfb84fa2c5db9d136
lslipski <lslipski602@gmail.com>
2020-03-29 17:13:28 -0400

    update to koban 2019 create canlab dataset script to keep all nps conditions and contrasts in object creation

--- koban_2019_scebl_social_pain/scripts/create_canlab_dataset_koban_2019.m
+++ koban_2019_scebl_social_pain/scripts/create_canlab_dataset_koban_2019.m
@@ -1,9 +1,6 @@
 % This script sets up the canlab_dataset object for the data in this folder.
 % For more information see 'help canlab_dataset' in CanlabCore
 %% Set up paths and load data
-
-% Woo et al., Distinct Brain Systems Mediate the Effects of Nociceptive Input and Self-Regulation on Pain
-
 basedir = '/Users/lukie/Documents/canlab/NPSSR/npssr_code/koban_2019_scebl_social_pain/';
 datadir = fullfile(basedir, 'results');
 
@@ -30,19 +27,41 @@ DAT.Description.Missing_Values = NaN;
 DAT.Subj_Level.id = nps_table.subjids;
 
 % Other variables
-DAT.Subj_Level.names = {'NPS_vals'}; 
+DAT.Subj_Level.names = {'Cues CSHigh'
+                        'Cues CSLow'
+                        'Cues SocHigh'
+                        'Cues SocLow'
+                        'Pain CSHigh'
+                        'Pain CSLow'
+                        'Pain SocHigh'
+                        'Pain SocLow'
+                        'Cues CShilo'
+                        'Cues Sochilo'
+                        'Pain CShilo'
+                        'Pain Sochilo'}; 
 
 
 % Descriptions, added to DAT.Subj_Level.descrip
 % -------------------------------------------------------------------------
 
-descrip = {'NPS values for SocialHigh > SocialLow'};
+descrip = {'NPS Condition response during CS High cue'
+           'NPS Condition response during CS Low cue'
+           'NPS Condition response during Social High cue'
+           'NPS Condition response during Social Low cue'
+           'NPS Condition response during CS High pain'
+           'NPS Condition response during CS Low pain'
+           'NPS Condition response during Social High pain'
+           'NPS Condition response during Social Low pain'
+           'NPS Contrast response CS High > Low during cue'
+           'NPS Contrast response Social High > Low during cue'
+           'NPS Contrast response CS High > Low during pain'
+           'NPS Contrast response Social High > Low during pain'};
         
 DAT.Subj_Level.descrip = descrip;
 
 %% ADD DATA to canlab_dataset object
 % -------------------------------------------------------------------------
-DAT.Subj_Level.data = table2array(nps_table(:,2));
+DAT.Subj_Level.data = table2array(nps_table(:,1:12));
 
 
 % Add variable types
