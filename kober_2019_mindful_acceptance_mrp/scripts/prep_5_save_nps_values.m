427e1d0599f8ba7548a6725bc0e1131f7266333b
lslipski <lslipski602@gmail.com>
2020-04-14 10:52:00 -0400

    adding prep_5_save_nps_values.m to mindful acceptance scripts

---
+++ kober_2019_mindful_acceptance_mrp/scripts/prep_5_save_nps_values.m
@@ -0,0 +1,33 @@
+% Grab NPS values from DAT (created in prep_xxx scripts).
+T = [DAT.npscontrasts]
+
+   
+
+%% get subjids from the imagine down image names (same for all 3 conditions)
+subjids = {'MRP01'
+'MRP02'
+'MRP03'
+'MRP04'
+'MRP05'
+'MRP06'
+'MRP07'
+'MRP08'
+'MRP10'
+'MRP11'
+'MRP12'
+'MRP13'
+'MRP14'
+'MRP16'
+'MRP17'
+'MRP18'};
+
+% add conditions as variable names to the nps values table and save to
+% /results subfolder
+nps_table = array2table(T,'VariableNames',DAT.contrastnames)
+newnames = 'subjids'
+nps_table = addvars(nps_table, subjids', 'NewVariableNames', newnames) % add subject IDs as column
+
+
+save(fullfile(resultsdir,'npsvals_bmrk3.mat'), 'nps_table') % save
+printhdr('Saved npsvalues')
+sprintf('Results Directory: %s', resultsdir)
