168f885f5d35c5daaf281bf94c602d7ece976509
lslipski <lslipski602@gmail.com>
2020-05-21 11:07:58 -0400

    edited create_canlab_multistudy to produce a zscored dataset as well

--- multistudy/scripts/create_canlab_dataset_multistudy.m
+++ multistudy/scripts/create_canlab_dataset_multistudy.m
@@ -31,6 +31,7 @@ end
              
 % initialize canlab dataset to hold all studies' data
 self_reg_results = canlab_dataset;
+self_reg_results_zscored = canlab_dataset;
 
 for i=1:size(dataset_names, 1)
     % load canlab dataset
@@ -41,28 +42,45 @@ for i=1:size(dataset_names, 1)
     %get contrast names and data
     ds_data_nps = DAT.Subj_Level.data(:, dataset_contrast_indices{i}(1));
     ds_data_beh = DAT.Subj_Level.data(:, dataset_contrast_indices{i}(2));
-    ds_names = {DAT.Subj_Level.names{[dataset_contrast_indices{i}]}}
-    ds_descrip = {DAT.Subj_Level.descrip{dataset_contrast_indices{i}}}
+    ds_names = {DAT.Subj_Level.names{[dataset_contrast_indices{i}]}};
+    ds_descrip = {DAT.Subj_Level.descrip{dataset_contrast_indices{i}}};
+    
+    % get z-scored data
+    ds_data_nps_z = zscore(ds_data_nps);
+    ds_data_beh_z = zscore(ds_data_beh);
+    
     
     % append NaN to datasets with fewer than the max subjects to account
     % for differences in sample size between datasets
     if n < max_subjects
-        nan_to_add = NaN(max_subjects - n, 1)
-        ds_data_nps = cat(1, ds_data_nps, nan_to_add)
-        ds_data_beh = cat(1, ds_data_beh, nan_to_add)
+        nan_to_add = NaN(max_subjects - n, 1);
+        ds_data_nps = cat(1, ds_data_nps, nan_to_add);
+        ds_data_beh = cat(1, ds_data_beh, nan_to_add);
+        ds_data_nps_z = cat(1, ds_data_nps_z, nan_to_add);
+        ds_data_beh_z = cat(1, ds_data_beh_z, nan_to_add);
     end
 
-    % append to canlab dataset vars
-    self_reg_results.Subj_Level.data = [self_reg_results.Subj_Level.data ds_data_nps ds_data_beh]
-    self_reg_results.Subj_Level.names = [self_reg_results.Subj_Level.names ds_names]
-    self_reg_results.Subj_Level.descrip = [self_reg_results.Subj_Level.descrip ds_descrip]
+    % append to canlab dataset vars non z-scored
+    self_reg_results.Subj_Level.data = [self_reg_results.Subj_Level.data ds_data_nps ds_data_beh];
+    self_reg_results.Subj_Level.names = [self_reg_results.Subj_Level.names ds_names];
+    self_reg_results.Subj_Level.descrip = [self_reg_results.Subj_Level.descrip ds_descrip];
+    
+    % append to canlab dataset vars z-scored
+    self_reg_results_zscored.Subj_Level.data = [self_reg_results_zscored.Subj_Level.data ds_data_nps_z ds_data_beh_z];
+    self_reg_results_zscored.Subj_Level.names = [self_reg_results_zscored.Subj_Level.names ds_names];
+    self_reg_results_zscored.Subj_Level.descrip = [self_reg_results_zscored.Subj_Level.descrip ds_descrip];
 
 end
 
-DAT = self_reg_results
 
-% save canlab dataset
+% save non zscored canlab dataset
+DAT = self_reg_results;
 cd(results_dir);
 get_var(self_reg_results);
 save self_reg_combined DAT
+
+% save zscored canlab dataset
+DAT = self_reg_results_zscored;
+get_var(self_reg_results_zscored);
+save self_reg_combined_zscored DAT
     
