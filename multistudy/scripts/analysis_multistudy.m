3a559fdde8c7df608ea37c8522f28f23d2282867
lslipski <lslipski602@gmail.com>
2020-09-07 14:57:52 -0400

    initial commit of create_multistudy_contrasts_dataframe to compile datasets into cell array

--- multistudy/scripts/analysis_multistudy.m
+++ multistudy/scripts/analysis_multistudy.m
@@ -5,8 +5,8 @@ cd(resultsdir);
 load('self_reg_combined.mat');
 
 %put into mydat so we can reformat
-[n, k] = size(mydat);
 mydat = DAT.Subj_Level.data;
+[n, k] = size(mydat);
 
 
 
@@ -19,11 +19,8 @@ mydat = mydat ./ repmat(mad(mydat), n, 1);
  for col=1:size(sign_switch,2)
      mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
  end
-
-
-%% Produce Behavioral Violin Plots
-
-% Get, reorder, and plot behavioral data
+ 
+ % Get, reorder, and plot behavioral data
 % -------------------------------------------------------------------------
 connames = {'Remifentanil NPS'
              'Remifentanil Beh'
@@ -35,7 +32,7 @@ connames = {'Remifentanil NPS'
              'Symbolic Cues Beh'
              'Social Cue NPS'
              'Social Cue Beh'
-             'Minduful Acc NPS'
+             'Mindful Acc NPS'
              'Mindful Acc Beh'
              'Handholding NPS'
              'Handholding Beh'
@@ -47,46 +44,6 @@ d = nanmean(mydat) ./ nanstd(mydat);
 [ds, wh] = sort(abs(d), 'ascend');
 mydat = mydat(:, wh);
 connames = connames(wh);
-
-         
-% get new behavioral indices only
-beh_indices = find(contains(connames,'Beh'));
-beh_dat = mydat(:,beh_indices);
-beh_names = connames(beh_indices);
-    
-
-% how many significant?  For colors
-% ------------------------------------------------------------------------
-t = nanmean(beh_dat) ./ ste(beh_dat);
-p = 2 * tcdf(abs(t), sum(~isnan(beh_dat)) - 1, 'upper'); % two-tailed
-
-k2 = sum(p < .05);
-[n, k] = size(beh_dat);
-
-vnames = format_strings_for_legend(beh_names);
-
-xvals = 1:k;
-colors = repmat({[.5 .5 .7]}, 1, k - k2);        % non-significant 
-colors = [colors custom_colors([.7 0 0], [1 .7 0], k2)']; % significant
-
-create_figure('Behavioral Pain Ratings')
-[h1, s1] = barplot_columns(beh_dat,'x', xvals, 'colors', colors, 'nofig');
-set(gca, 'XTickLabel', vnames);
-ylabel('Pain ratings');
-title('Pain Rating by Study');
-
-pos = get(gcf, 'Position'); % get gcf gets the position of the current figure
-if pos(3) ./ pos(4) < 2
-    pos(3) = pos(3) .* 2.5;
-end
-set(gcf, 'Position', pos);
-
-drawnow, snapnow; %drawnow updates figures with new gcf; snapnow takes a snapshot for publishing
-
-%% Save Pain Plot
-% -------------------------------------------------------------------------
-saveas(gcf, fullfile(figuresdir, 'ratings_violin_with_points.fig'));
-
 
 %% Produce NPS Violin Plots
 % get new behavioral indices only
@@ -113,6 +70,7 @@ create_figure('NPS Responses by Study')
 [h1, s1] = barplot_columns(nps_dat,'x', xvals, 'colors', colors, 'nofig');
 set(gca, 'XTickLabel', vnames);
 ylabel('NPS Response');
+xlabel('Study');
 title('NPS Response by Study');
 
 pos = get(gcf, 'Position'); % get gcf gets the position of the current figure
@@ -123,42 +81,80 @@ set(gcf, 'Position', pos);
 
 drawnow, snapnow; %drawnow updates figures with new gcf; snapnow takes a snapshot for publishing
 
+%% Save NPS Plot
+% -------------------------------------------------------------------------
+saveas(gcf, fullfile(figuresdir, 'multistudy_7_NPS_violin_with_points.fig'));
+saveas(gcf, fullfile(figuresdir, 'multistudy_7_NPS_violin_with_points.png'));
 
+%% Produce Behavioral Violin Plots
+% get new behavioral names in same order as NPS plot
+beh_names = []
+for i = 1:size(nps_names,1)
+    beh_names = [beh_names strrep(nps_names(i),'NPS', 'Beh')];
+end
+beh_names = beh_names'; % transpose to match connames dimensions
 
+beh_indices = []
+% get indicies based on order of names
+for p = 1:size(beh_names,1)
+    beh_indices = [beh_indices find(contains(connames,beh_names(p)))];
+end 
 
+% transpose to match mydata dimensions
+beh_indices = beh_indices';
+% get data based on indices
+beh_dat = mydat(:,beh_indices);
 
+% how many significant?  For colors
+% ------------------------------------------------------------------------
+t = nanmean(beh_dat) ./ ste(beh_dat);
+p = 2 * tcdf(abs(t), sum(~isnan(beh_dat)) - 1, 'upper'); % two-tailed
 
+k2 = sum(p < .05);
+[n, k] = size(beh_dat);
 
+vnames = format_strings_for_legend(beh_names);
 
+xvals = 1:k;
+colors = repmat({[.5 .5 .7]}, 1, k - k2);        % non-significant 
+colors = [colors custom_colors([.7 0 0], [1 .7 0], k2)']; % significant
 
+create_figure('Behavioral Pain Ratings')
+[h1, s1] = barplot_columns(beh_dat,'x', xvals, 'colors', colors, 'nofig');
+set(gca, 'XTickLabel', vnames);
+ylabel('Pain ratings');
+xlabel('Study');
+title('Pain Rating by Study');
 
+pos = get(gcf, 'Position'); % get gcf gets the position of the current figure
+if pos(3) ./ pos(4) < 2
+    pos(3) = pos(3) .* 2.5;
+end
+set(gcf, 'Position', pos);
 
+drawnow, snapnow; %drawnow updates figures with new gcf; snapnow takes a snapshot for publishing
 
-
-
-
-
-
-
+%% Save Pain Plot
+% -------------------------------------------------------------------------
+saveas(gcf, fullfile(figuresdir, 'multistudy_7_ratings_violin_with_points.fig'));
+saveas(gcf, fullfile(figuresdir, 'multistudy_7_ratings_violin_with_points.png'));
 
 
 %% Produce ranked dataset
-ds = load('self_reg_combined.mat');
-[r,c] = size(ds.DAT.Subj_Level.data);
-
-% get only non NaN values and rank them
-for i = 1:c
-    real_indicies = (~isnan(ds.DAT.Subj_Level.data(:,i)));
-    mydat = ds.DAT.Subj_Level.data(real_indicies,i);
-    myrankdat = rankdata(mydat);
-    nan_to_add = NaN(r - size(mydat,1), 1);
-    myrankdat = cat(1, myrankdat, nan_to_add);
-    ds.DAT.Subj_Level.data(:,i) = myrankdat;
-end
-
-% get spearman correlations for NPS + pain ratings pairs
-for col = 1:2:size(ds.DAT.Subj_Level.data,2)
-    subdata = ds.DAT.Subj_Level.data(:,col:col+1)
-end
-
-
+% ds = load('self_reg_combined.mat');
+% [r,c] = size(ds.DAT.Subj_Level.data);
+% 
+% % get only non NaN values and rank them
+% for i = 1:c
+%     real_indicies = (~isnan(ds.DAT.Subj_Level.data(:,i)));
+%     mydat = ds.DAT.Subj_Level.data(real_indicies,i);
+%     myrankdat = rankdata(mydat);
+%     nan_to_add = NaN(r - size(mydat,1), 1);
+%     myrankdat = cat(1, myrankdat, nan_to_add);
+%     ds.DAT.Subj_Level.data(:,i) = myrankdat;
+% end
+% 
+% % get spearman correlations for NPS + pain ratings pairs
+% for col = 1:2:size(ds.DAT.Subj_Level.data,2)
+%     subdata = ds.DAT.Subj_Level.data(:,col:col+1)
+% end
---
+++ multistudy/scripts/create_multistudy_contrasts_dataframe.m
@@ -0,0 +1,91 @@
+%% Create Between Subject Table with only Contrasts of Interest
+
+basedir = '/Users/lukie/Documents/canlab/NPSSR/npssr_code';
+results_dir = fullfile(basedir,'multistudy','results')
+
+max_subjects = 36;  % maximum numer of subjects in any of the given studies
+
+% list of folders to look through for nps and ratings data
+dataset_names = {%'atlas_2013_remi_open_hidden'
+                  'becker_2017_pain_reward'
+                  'bmrk3'
+                  'jepma_2018_ie2'
+%                  'koban_2019_scebl_social_pain'
+                  'kober_2019_mindful_acceptance_mrp'
+                  'lopezsola_2019_handholding_pain'
+                 %'ma_2016_pain_citalopram'
+                 %'roy_emomod_2009'
+                 };
+             
+dataset_keys = {%'remi'
+                  'pain_reward'
+                  'bmrk3'
+                  'ie2'
+%                  'social_pain'
+                  'mindful_acceptance'
+                  'handholding'
+                 %'ma_2016_pain_citalopram'
+                 %'roy_emomod_2009'
+                 };
+             
+% initialize canlab dataset to hold all studies' data
+multistudy_contrasts_data_scaled = {};
+multistudy_contrasts_names={};
+                        
+for i=1:size(dataset_names, 1)
+    % load dataset from project folder
+    cd(fullfile(basedir,dataset_names{i}, 'results'))
+    contrasts = load(fullfile(pwd, 'contrast_data_objects.mat'));
+    
+    %get column headers from image_names and preprend study key
+    contrast_names = contrasts.DATA_OBJ_CONsc{1}.image_names;
+    [worthless, len] = size(contrast_names);
+    this_data = [];
+    for q = 1:len
+        this_contrast = strcat(dataset_keys(i),'_', contrast_names{q});
+        this_data = contrasts.DATA_OBJ_CONsc{q};
+        [n, k] = size(this_data);
+        
+        
+        % add contrast data and name to fmri_dataset
+        fmri_ds = this_data;
+        fmri_ds.image_names = this_contrast;
+        
+        % print size info for this contrast
+        disp('Adding data from: ')
+        disp(dataset_keys{i})
+        disp('Contrast being added: ')
+        disp(fmri_ds.image_names)
+        disp('Of size: ')
+        disp(size(fmri_ds.dat))
+        
+        % add newly formatted contrast to full dataset
+        multistudy_contrasts_data_scaled{end+1} = fmri_ds;
+        multistudy_contrasts_names{end+1} = fmri_ds.image_names;
+        
+    end
+
+end
+
+
+% save multistudy files
+
+multistudy_contrasts_scaled.data = multistudy_contrasts_data_scaled;
+multistudy_contrasts_scaled.contrast_names = multistudy_contrasts_names;
+
+savename = strcat('multistudy_contrasts_scaled_', date, '.mat')
+save(fullfile(results_dir, savename), 'multistudy_contrasts_scaled') % save
+
+
+cd(results_dir)
+
+
+
+
+
+
+
+
+
+
+
