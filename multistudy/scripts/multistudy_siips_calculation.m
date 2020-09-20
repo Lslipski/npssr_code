e0a92c74d264c911328e707105c817348a224a7d
lslipski <lslipski602@gmail.com>
2020-09-20 17:24:04 -0400

    initial commit for scripts to create non-scaled multistudy contrasts dataframe and calculate siips

--- multistudy/scripts/multistudy_siips_calculation.m
+++ multistudy/scripts/multistudy_siips_calculation.m
@@ -1,6 +1,6 @@
 % this script uses the apply_siips method to get scalar outputs for each
 % participant in each contrast in each study of the
-% multistudy_contrasts_scaled 
+% multistudy_contrasts 
 
 % set paths
 code_dir = '/Users/lukie/Documents/canlab/NPSSR/npssr_code/multistudy/scripts';
@@ -8,8 +8,9 @@ results_dir = '/Users/lukie/Documents/canlab/NPSSR/npssr_code/multistudy/results
 cd(results_dir);
 
 % load multistudy data set
-load('multistudy_contrasts_scaled_07-Sep-2020.mat')
-dat = multistudy_contrasts_scaled.data;
+% need to change to multistudy_contrasts_scaled.data if need scaled version
+load('multistudy_contrasts_20-Sep-2020.mat')
+dat = multistudy_contrasts.data;
 num_cons = size(dat, 2);
 
 % initialize cell array to hold SIIPS output values of length equal to
@@ -19,14 +20,14 @@ siips_results = cell(1,num_cons);
 % print names of contrasts
 % loop through contrasts, apply siips, save results
 for i=1:num_cons
-    disp(multistudy_contrasts_scaled.contrast_names{i})
+    disp(multistudy_contrasts.contrast_names{i})
     siips_results(i) = apply_siips(dat(i));
 end
 
 % save multistudy files
 
 multistudy_siips_results.data = siips_results;
-multistudy_siips_results.contrast_names = multistudy_contrasts_scaled.contrast_names;
+multistudy_siips_results.contrast_names = multistudy_contrasts.contrast_names;
 
 savename = strcat('multistudy_siips_results_', date, '.mat');
 save(fullfile(results_dir, savename), 'multistudy_siips_results'); % save
@@ -38,4 +39,4 @@ disp(size(siips_results));
 disp('Saved to: ');
 disp(results_dir);
 disp('You are in: ');
-disp(code_
+disp(code_dir);
