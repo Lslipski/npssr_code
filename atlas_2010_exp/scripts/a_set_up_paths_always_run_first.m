9af07d984cf0bd6e8d6c39a9ee7a55a3a4834bfb
lslipski <lslipski602@gmail.com>
2020-12-22 17:33:32 -0500

    initial commit of prep scripts to run canlab batch scripts on exp data

---
+++ atlas_2010_exp/scripts/a_set_up_paths_always_run_first.m
@@ -0,0 +1,61 @@
+% Always run this first before you run other scripts.
+%
+% NOTES:
+% - standard folders and variable names are created by these scripts
+%
+% - in "prep_" scripts: 
+%   image names, conditions, contrasts, colors, global gray/white/CSF
+%   values are saved automatically in a DAT structure
+% 
+% - extracted fmri_data objects are saved in DATA_OBJ variables
+% - contrasts are estimated and saved in DATA_OBJ_CON variables
+%
+% - files with these variables are saved and loaded automatically when you
+%   run the scripts
+%   meta-data saved in image_names_and_setup.mat
+%   image data saved in data_objects.mat
+%
+% - you only need to run the prep_ scripts once.  After that, use 
+%   b_reload_saved_matfiles.m to re-load saved files
+% 
+% - when all scripts working properly, run z_batch_publish_analyses.m
+%   to create html report.  customize by editing z_batch_list_to_publish.m
+%
+% - saved in results folder:
+%   figures
+%   html report with figures and stats, in "published_output"
+
+% Set base directory
+% --------------------------------------------------------
+
+% Base directory for whole study/analysis
+basedir = '/Users/lukie/Documents/canlab/NPSSR/npssr_code/atlas_2010_exp';
+datadir = '/Users/lukie/Documents/canlab/NPSSR/NPSSR_contrast_images_local/atlas_2010_exp/conditions';
+
+
+% Set user options
+% --------------------------------------------------------
+
+a2_set_default_options
+
+% Set up paths
+% --------------------------------------------------------
+
+cd(basedir)
+
+resultsdir = fullfile(basedir, 'results');
+scriptsdir = fullfile(basedir, 'scripts');
+figsavedir = fullfile(resultsdir, 'figures');
+notesdir = fullfile(resultsdir, 'notes');
+
+addpath(scriptsdir)
+
+if ~exist(resultsdir, 'dir'), mkdir(resultsdir); end
+if ~exist(figsavedir, 'dir'), mkdir(figsavedir); end
+
+% Display helper functions: Called by later scripts
+% --------------------------------------------------------
+
+dashes = '----------------------------------------------';
+printstr = @(dashes) disp(dashes);
+printhdr = @(str) fprintf('%s\n%s\n%s\n', dashes, str, dashes);
