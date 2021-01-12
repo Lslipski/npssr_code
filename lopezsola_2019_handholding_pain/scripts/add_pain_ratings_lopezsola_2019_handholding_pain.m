e793086b2c4850ec1b644d97a84b95ed458e8777
lslipski <lslipski602@gmail.com>
2021-01-12 17:18:32 -0500

    correcting handholding ratings contrast so it appears negative in plot

--- lopezsola_2019_handholding_pain/scripts/add_pain_ratings_lopezsola_2019_handholding_pain.m
+++ lopezsola_2019_handholding_pain/scripts/add_pain_ratings_lopezsola_2019_handholding_pain.m
@@ -3,28 +3,28 @@ a_set_up_paths_always_run_first;
 
 cd(resultsdir)
 
-load canlab_dataset_lopezsola_2019_handholding_pain.mat DAT
+%load canlab_dataset_lopezsola_2019_handholding_pain.mat DAT
 
 % only using intensity ratings; unpleasantness ratings are in BG:BH for baseline
 % and handholding respectively
 cd(datadir)
 ratings = xlsread('HH_behav_Jan2016_Marianne_03252019.xlsx',1,'BE:BF');
 
-
+mydat = canlab_dataset;
 %% Add pain ratings to new canlab data object
 % label to distinguish behavioral ratings from nps values
 ratings_descrip = {'Behavioral Rating Baseline'
                    'Behavioral Rating Handholding'};
                
-DAT.Subj_Level.descrip = [DAT.Subj_Level.descrip; ratings_descrip];
+mydat.Subj_Level.descrip = [mydat.Subj_Level.descrip; ratings_descrip];
 
 % add descriptions for each behavioral condition
 ratings_names = {'Beh_baseline'
                  'Beh_handholding'};
-DAT.Subj_Level.names = [DAT.Subj_Level.names; ratings_names];
+mydat.Subj_Level.names = [mydat.Subj_Level.names; ratings_names];
 
 % add raw ratings data to Subject level data 
-DAT.Subj_Level.data = [DAT.Subj_Level.data ratings];
+mydat.Subj_Level.data = [mydat.Subj_Level.data ratings];
 
 %% add contrasts for pain ratings
 
@@ -33,20 +33,20 @@ DAT.Subj_Level.data = [DAT.Subj_Level.data ratings];
 C = [-1 1]';
 condescrips = {'Behavioral Contrast Handholding > Baseline'};
 connames = {'beh_con_HH_vs_BL'};
-DAT.Subj_Level.descrip = [DAT.Subj_Level.descrip; condescrips];
-DAT.Subj_Level.names = [DAT.Subj_Level.names; connames];
+mydat.Subj_Level.descrip = [mydat.Subj_Level.descrip; condescrips];
+mydat.Subj_Level.names = [mydat.Subj_Level.names; connames];
 
-mydat = get_var(DAT, DAT.Subj_Level.names(4:5)); % get subject level data for each of the behavioral conditions
-convals = mydat * C; % multiply the data by the contrasts to get behavioral values for each subject for each contrast
+condat = mydat.Subj_Level.data; % get subject level data for each of the behavioral conditions
+convals = condat * C; % multiply the data by the contrasts to get behavioral values for each subject for each contrast
 
 %concatenate new contrast values to data field
-DAT.Subj_Level.data = [DAT.Subj_Level.data convals];
+mydat.Subj_Level.data = [mydat.Subj_Level.data convals];
 
 % Add variable types
-k = length(DAT.Subj_Level.names); % get subject level name length
-DAT.Subj_Level.type = repmat({'numeric'}, 1, k); % get variable types
+k = length(mydat.Subj_Level.names); % get subject level name length
+mydat.Subj_Level.type = repmat({'numeric'}, 1, k); % get variable types
 
-
+DAT = mydat;
 %% List variable names
 cd(resultsdir)
 
