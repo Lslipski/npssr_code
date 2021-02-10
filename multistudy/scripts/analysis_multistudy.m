96d11ef3095e88306be1c8649c703098ad9eda5a
lslipski <lslipski602@gmail.com>
2021-02-09 23:10:14 -0500

    updated analysis for prelim results violin plots and scatterplot

--- multistudy/scripts/analysis_multistudy.m
+++ multistudy/scripts/analysis_multistudy.m
@@ -72,10 +72,10 @@ sum(~isnan(ms_nps_all))
 %% Pain Ratings First
 mydat = ms_ratings_all;
 connames = ms_ratings_tab.Properties.VariableNames;
-con2plot = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Controllability', 'Imagination', 'Emotion', 'Handholding', 'Mindfulness', 'Meaning'};
+con2plot = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Imagination', 'Emotion', 'Handholding', 'Mindfulness', 'Meaning'};
 
 % Make all pain reductions negative
-sign_switch = [1 2 6 3 7];
+sign_switch = [1 2 3 5];
  
  for col=1:size(sign_switch,2)
      mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
@@ -121,14 +121,16 @@ drawnow, snapnow; %drawnow updates figures with new gcf; snapnow takes a snapsho
 %% NPS Values Second
 mydat = ms_nps_all;
 connames = ms_brain_labels;
-con2plot = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Controllability', 'Imagination', 'Emotion','Handholding', 'Mindfulness','Social', 'Meaning'};
+%con2plot = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Controllability', 'Imagination', 'Emotion','Handholding', 'Mindfulness','Social', 'Meaning'};
+con2plot = {'Symbolic Cond.', 'Meaning', 'Social', 'Emotion', 'Controllability', 'Handholding', 'Expectation', 'Mindfulness', 'Remifentanil', 'Reward', 'Imagination'};
 
 % Make imagination, social, and symbolic cues negative effects to match others
-% sign_switch = [ 2 6 3 7];
-%  
-%  for col=1:size(sign_switch,2)
-%      mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
-%  end
+%sign_switch = [2 3 4 5 6 11];
+sign_switch = [3 11 2 5 4 6];
+ 
+ for col=1:size(sign_switch,2)
+     mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
+ end
 
 % Sort by effect size
 % ------------------------------------------------------------------------
@@ -169,17 +171,44 @@ drawnow, snapnow;
 
 
 %% scatterplot of brain by behavior
-mydatpain_avg = nanmean(mydatpain)
-mydatnps_avg = nanmean(mydat)
+%grab pain ratings
+mydat = ms_ratings_all;
+connames = ms_ratings_tab.Properties.VariableNames;
+mynames_pain = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Imagination', 'Emotion', 'Handholding', 'Mindfulness', 'Meaning'};
+
+% Make all pain reductions negative
+sign_switch = [1 2 3 5];
+ 
+ for col=1:size(sign_switch,2)
+     mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
+ end
+
+% average pain rating by study
+mydatpain_avg = nanmean(mydat);
+
+
+% grab nps responses
+mydat = ms_nps_all;
+connames = ms_brain_labels;
+%con2plot = {'Remifentanil', 'Expectation', 'Symbolic Cond.', 'Reward', 'Controllability', 'Imagination', 'Emotion','Handholding', 'Mindfulness','Social', 'Meaning'};
+con2plot = {'Symbolic Cond.', 'Meaning', 'Social', 'Emotion', 'Controllability', 'Handholding', 'Expectation', 'Mindfulness', 'Remifentanil', 'Reward', 'Imagination'};
+
+sign_switch = [3 11 2 5 4 6];
+ 
+ for col=1:size(sign_switch,2)
+     mydat(:,sign_switch(col)) = mydat(:,sign_switch(col)) * -1;
+ end
+
+mydat_nps_scatter = mydat(:,[9 7 1 10 11 4 6 8 2]);
+mynames_nps = con2plot([9 7 1 10 11 4 6 8 2]);
+mydatnps_avg = nanmean(mydat_nps_scatter)
+
 plot_correlation_samefig(mydatpain_avg, mydatnps_avg)
-figure
-plot_correlation_samefig(mydatpain_avg, mydatnps_avg)
-cla
-plot_correlation_samefig(mydatpain_avg', mydatnps_avg')
 
-
-
-
+disp('Pain Contrasts: ')
+mynames_pain'
+disp('NPS Contrasts: ')
+mynames_nps'
 
 
 
@@ -189,76 +218,3 @@ plot_correlation_samefig(mydatpain_avg', mydatnps_avg')
 % -------------------------------------------------------------------------
 saveas(gcf, fullfile(figuresdir, 'multistudy_7_NPS_violin_with_points.fig'));
 saveas(gcf, fullfile(figuresdir, 'multistudy_7_NPS_violin_with_points.png'));
-
-%% Produce Behavioral Violin Plots
-% get new behavioral names in same order as NPS plot
-beh_names = []
-for i = 1:size(nps_names,1)
-    beh_names = [beh_names strrep(nps_names(i),'NPS', 'Beh')];
-end
-beh_names = beh_names'; % transpose to match connames dimensions
-
-beh_indices = []
-% get indicies based on order of names
-for p = 1:size(beh_names,1)
-    beh_indices = [beh_indices find(contains(connames,beh_names(p)))];
-end 
-
-% transpose to match mydata dimensions
-beh_indices = beh_indices';
-% get data based on indices
-beh_dat = mydat(:,beh_indices);
-
-% how many significant?  For colors
-% ------------------------------------------------------------------------
-t = nanmean(beh_dat) ./ ste(beh_dat);
-p = 2 * tcdf(abs(t), sum(~isnan(beh_dat)) - 1, 'upper'); % two-tailed
-
-k2 = sum(p < .05);
-[n, k] = size(beh_dat);
-
-vnames = format_strings_for_legend(beh_names);
-
-xvals = 1:k;
-colors = repmat({[.5 .5 .7]}, 1, k - k2);        % non-significant 
-colors = [colors custom_colors([.7 0 0], [1 .7 0], k2)']; % significant
-
-create_figure('Behavioral Pain Ratings')
-[h1, s1] = barplot_columns(beh_dat,'x', xvals, 'colors', colors, 'nofig');
-set(gca, 'XTickLabel', vnames);
-ylabel('Pain ratings');
-xlabel('Study');
-title('Pain Rating by Study');
-
-pos = get(gcf, 'Position'); % get gcf gets the position of the current figure
-if pos(3) ./ pos(4) < 2
-    pos(3) = pos(3) .* 2.5;
-end
-set(gcf, 'Position', pos);
-
-drawnow, snapnow; %drawnow updates figures with new gcf; snapnow takes a snapshot for publishing
-
-%% Save Pain Plot
-% -------------------------------------------------------------------------
-saveas(gcf, fullfile(figuresdir, 'multistudy_7_ratings_violin_with_points.fig'));
-saveas(gcf, fullfile(figuresdir, 'multistudy_7_ratings_violin_with_points.png'));
-
-
-%% Produce ranked dataset
-% ds = load('self_reg_combined.mat');
-% [r,c] = size(ds.DAT.Subj_Level.data);
-% 
-% % get only non NaN values and rank them
-% for i = 1:c
-%     real_indicies = (~isnan(ds.DAT.Subj_Level.data(:,i)));
-%     mydat = ds.DAT.Subj_Level.data(real_indicies,i);
-%     myrankdat = rankdata(mydat);
-%     nan_to_add = NaN(r - size(mydat,1), 1);
-%     myrankdat = cat(1, myrankdat, nan_to_add);
-%     ds.DAT.Subj_Level.data(:,i) = myrankdat;
-% end
-% 
-% % get spearman correlations for NPS + pain ratings pairs
-% for col = 1:2:size(ds.DAT.Subj_Level.data,2)
-%     subdata = ds.DAT.Subj_Level.data(:,col:col+1)
-% end
